8e4274e995cfca3092c59ed3e27a4ce9666ff8bb
casti <joaovictor27cm@gmail.com>
2026-02-24 19:57:33 -0300

    Iniciando a aplicação principal

--- src/chapter12_composition/application/Program.java
+++ src/chapter12_composition/application/Program.java
@@ -1,9 +1,19 @@
 package chapter12_composition.application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter departament's name:");
+		String departmentName = sc.nextLine();
+		System.out.print("Enter worker data:");
 
 	}
 
