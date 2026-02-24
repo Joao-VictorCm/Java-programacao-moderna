306f64e5767c31281315d3bb2e73a95e74d5357e
casti <joaovictor27cm@gmail.com>
2026-02-24 20:23:28 -0300

    desenvolvendo o sistema principal

--- src/chapter12_composition/application/Program.java
+++ src/chapter12_composition/application/Program.java
@@ -1,19 +1,51 @@
 package chapter12_composition.application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
+import chapter12_composition.entities.Department;
+import chapter12_composition.entities.HourContract;
+import chapter12_composition.entities.Worker;
+import chapter12_composition.entities.enums.WorkerLevel;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		// TODO Auto-generated method stub
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		System.out.print("Enter departament's name:");
 		String departmentName = sc.nextLine();
 		System.out.print("Enter worker data:");
+		System.out.print("Name:");
+		String workerName = sc.nextLine();
+		System.out.print("Level:");
+		String workerLevel = sc.nextLine();
+		System.out.print("Base salary:");
+		double baseSalary = sc.nextDouble();
+		
+		Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel) , baseSalary, new Department(departmentName));
+		
+		System.out.println("How many contracts to this worker?");
+		int n = sc.nextInt();
+		
+		for(int i=0; i < n; i++) {
+			System.out.println("Enter contract #" + i + " data:");
+			System.out.println("Date (DD/MM/YYYY): ");
+			Date contractDate = sdf.parse(sc.next());
+			System.out.println("Value per hour: ");
+			double valuePerHour = sc.nextDouble();
+			System.out.println("Duration (hours): ");
+			int hours = sc.nextInt();
+			HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+			worker.addContract(contract); //para ligar os contratos a classe trabalhador
+		}
 
 	}
 
